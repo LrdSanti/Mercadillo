1f92010197bcf8062a08905151b44021e4ff92f1
LrdSanti <hernandez.santiago.5367@eam.edu.co>
2023-05-19 17:32:29 -0500

    Proyecto Inicial

---
+++ src/main/java/com/mall/mercadillo/MercadilloApplication.java
@@ -0,0 +1,13 @@
+package com.mall.mercadillo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadilloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadilloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mall/mercadillo/MercadilloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.mercadillo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadilloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
