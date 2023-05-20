a8f74eea7a2ad84eff9ea84f37b946d35c73ad7d
LrdSanti <hernandez.santiago.5367@eam.edu.co>
2023-05-19 20:33:22 -0500

    Autorizacion configurada

--- src/main/java/com/mall/mercadillo/MercadilloApplication.java
+++ src/main/java/com/mall/mercadillo/MercadilloApplication.java
@@ -2,8 +2,9 @@ package com.mall.mercadillo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class MercadilloApplication {
 
 	public static void main(String[] args) {
