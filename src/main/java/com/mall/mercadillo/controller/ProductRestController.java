3b595f152b04054442a5bd71579f804869b2bed6
LrdSanti <hernandez.santiago.5367@eam.edu.co>
2023-05-26 21:28:48 -0500

    Firmas de metodos producto

--- src/main/java/com/mall/mercadillo/controller/ProductRestController.java
+++ src/main/java/com/mall/mercadillo/controller/ProductRestController.java
@@ -1,5 +1,15 @@
 package com.mall.mercadillo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mall.mercadillo.service.IProductService;
+
+@RestController
+@RequestMapping("/api/v1")
 public class ProductRestController {
     
+    @Autowired
+    public IProductService productService;
 }
